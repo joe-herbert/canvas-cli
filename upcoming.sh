9a33feeeade67e7e1fa0076f913c512868eddb00
Joe Herbert <joecherbert@me.com>
2021-03-10 01:29:36 +0000

    Combine scripts into one to make installation easier

--- upcoming.sh
+++ upcoming.sh
@@ -6,13 +6,13 @@ blankMode=false
 onlyFavCourses=false
 includeLocked=false
 includeSubmitted=false
-filterModules=false
-moduleList=()
+filterCourses=false
+courseList=()
 
 while getopts ":hcbflsm:" opt; do
     case $opt in
         h ) echo -e "\nUsage:\n\tcanvas u [option]\n\tcanvas upcoming [option]"
-            echo -e "Options:\n\t-h,Display this help message.\n\t-c,Display the output in compact style.\n\t-b,Enable blank mode so the output doesn't use colours.\n\t-f,Display only favourited courses.\n\t-l,Display assignments which haven't yet unlocked.\n\t-s,Display submitted assignments.\n\t-m [code],Display only specific modules. Modules should be specified as 3 digit numeric codes separated by a comma." | column -t -s ','
+            echo -e "Options:\n\t-h,Display this help message.\n\t-c,Display the output in compact style.\n\t-b,Enable blank mode so the output doesn't use colours.\n\t-f,Display only favourited courses.\n\t-l,Display assignments which haven't yet unlocked.\n\t-s,Display submitted assignments.\n\t-m [code],Display only specific courses. Courses should be specified as 3 digit numeric codes separated by a comma." | column -t -s ','
             echo -e "\nCanvas Token:\nYou must generate a canvas token to use this script. On Canvas, go to your Account Settings and look under Approved Integrations. Create a new access token with the name \"Canvas CLI\" and with no expiration date. Copy the token value, run any Canvas CLI command and paste the token when requested."
             exit 0
             ;;
@@ -26,16 +26,16 @@ while getopts ":hcbflsm:" opt; do
                     exit 1
                     ;;
                 "" ) ;;
-                * ) filterModules=true
-                    while IFS=',' read -ra modules; do
-                        for i in "${modules[@]}"; do
+                * ) filterCourses=true
+                    while IFS=',' read -ra courses; do
+                        for i in "${courses[@]}"; do
                             if [ "$i" != "" ]; then
-                                moduleList+=( ${i:0:3} )
+                                courseList+=( ${i:0:3} )
                             fi
                         done
                     done <<< $(echo "$OPTARG" | sed 's/[^0-9,]*//g')
-                    if [ ${#moduleList[@]} = "0" ]; then
-                        echo "Invalid module(s) '$OPTARG': see -h for more info" 1>&2
+                    if [ ${#courseList[@]} = "0" ]; then
+                        echo "Invalid course(s) '$OPTARG': see -h for more info" 1>&2
                         exit 1
                     fi
                     ;;
@@ -64,7 +64,7 @@ oneWeek=$(date -d "+10days" +"%s")
 colorsArray=()
 titlesArray=()
 deadlinesArray=()
-modulesArray=()
+coursesArray=()
 unlocksAtArray=()
 
 . loader.sh
@@ -77,7 +77,7 @@ for course in $(curl -s -H "Authorization: Bearer $CANVAS_TOKEN" "$COURSE_URL" |
     }
 
     course_code="$(_jqCourse '.course_code')"
-    if [ "$filterModules" = false ] || [[ " ${moduleList[@]} " =~ " ${course_code:4:3} " ]]; then
+    if [ "$filterCourses" = false ] || [[ " ${courseList[@]} " =~ " ${course_code:4:3} " ]]; then
     
         for row in $(curl -s -H "Authorization: Bearer $CANVAS_TOKEN" "$CANVAS_URL/api/v1/courses/$(_jqCourse '.id')/assignments?order_by=due_at&include[]=submission" | jq -r '.[] | @base64'); do
 
@@ -113,7 +113,7 @@ for course in $(curl -s -H "Authorization: Bearer $CANVAS_TOKEN" "$COURSE_URL" |
                         colorsArray+=( "$color" )
                         titlesArray+=( "$(_jqRow '.name')" )
                         deadlinesArray+=( "$(date -d $due_at +"%a %d %b %Y %H:%M")" )
-                        modulesArray+=( ${course_code:4:3} )
+                        coursesArray+=( ${course_code:4:3} )
                         if [ "$includeLocked" = true ]; then
                             if [ "$unlock_at" = "null" ] || [ "$(date -d $unlock_at +"%s")" -le $today ]; then
                                 unlocksAtArray+=( "" )
@@ -133,29 +133,29 @@ done
 #output
 tableHeaders=""
 if [ "$compact" = true ]; then
-    tableHeaders="Module,Title,Deadline"
+    tableHeaders="Course,Title,Deadline"
     if [ "$includeLocked" = true ]; then
         tableHeaders="$tableHeaders,Unlocks At"
     fi
 fi
-lastModule=""
+lastCourse=""
 printf "\033[1;2m"
 for ((i=0; i<${#titlesArray[@]}; i++)); do
-    if [ "$lastModule" != "${modulesArray[i]}" ]; then
-        if [ "$lastModule" != "" ]; then
+    if [ "$lastCourse" != "${coursesArray[i]}" ]; then
+        if [ "$lastCourse" != "" ]; then
             echo -e "||"
         fi
         if [ "$compact" = false ]; then
-            echo -e "\033[1;2m${modulesArray[i]}"
+            echo -e "\033[1;2m${coursesArray[i]}"
+            fi
+            lastCourse=${coursesArray[i]}
         fi
-        lastModule=${modulesArray[i]}
-    fi
     line="${titlesArray[i]}${NC}|${deadlinesArray[i]}"
     if [ "$blankMode" = false ]; then
         line="${colorsArray[i]}$line"
     fi
     if [ "$compact" = true ]; then
-        line="${NC}${modulesArray[i]}|$line"
+        line="${NC}${coursesArray[i]}|$line"
     fi
     if [ "$includeLocked" = true ]; then
         line="$line|${unlocksAtArray[i]}"
